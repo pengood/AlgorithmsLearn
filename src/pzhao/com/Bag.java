358d5acda8d94e6847b3a5370f1cdc28d7e92c1e
PeterZhao <zhaopeng_2009@163.com>
2015-03-23 14:56:50 +0800

    add Bag

---
+++ src/pzhao/com/Bag.java
@@ -0,0 +1,65 @@
+package pzhao.com;
+
+import java.util.Iterator;
+
+public class Bag<Item> implements Iterable<Item> {
+	
+	private class Node{
+		Item item;
+		Node next;
+		
+		public Node(Item item){
+			this.item=item;
+		}
+	}
+		private  Node first;
+		private int N;
+		public int size(){
+			return N;
+		}
+		public boolean isEmpty(){
+			return N==0;
+		}
+		public void add(Item item){
+			Node tmpNode=first;
+			first=new Node(item);
+			first.next=tmpNode;
+			N++;
+		}
+		
+
+	@Override
+	public Iterator<Item> iterator() {
+			
+		// TODO Auto-generated method stub
+		return new Iterator<Item>() {
+			Node tmp=first;
+			@Override
+			public boolean hasNext() {
+				// TODO Auto-generated method stub
+			
+				return tmp!=null;
+			}
+
+			@Override
+			public Item next() {
+				// TODO Auto-generated method stub
+				Item item=tmp.item;
+				tmp=tmp.next;
+				return item;
+			}
+		};
+	}
+	public static void main(String[] args){
+		Bag<String> bag=new Bag<>();
+		for(int i=0;i<10;i++){
+			bag.add("a"+i);
+		}
+		System.out.println(bag.size());
+		Iterator<String> iterator=bag.iterator();
+		while(iterator.hasNext()){
+			System.out.print(iterator.next()+" ");
+		}
+	}
+
+}
